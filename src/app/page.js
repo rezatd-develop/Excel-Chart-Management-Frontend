1efe3c1bc496766ad40177c0beb1fed775797490
rezatd-develop <reza.td.develop@gmail.com>
2026-07-22 11:23:26 +0330

    fix errors is production

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 'use client'
 
-import Image from "next/image";
-import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
-  router.push('/dashboard/overview/all-charts')
+    const router = useRouter();
 
-  return;
+    useEffect(() => {
+        router.replace('/dashboard/overview/all-charts');
+    }, [router]);
+
+    return null;
 }
